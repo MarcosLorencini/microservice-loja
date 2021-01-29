43022283bb909f79e8dba6d501e7147b10504e0f
MarcosLorencini <mlorencinif@gmail.com>
2021-01-29 17:42:27 -0300

    alterar a chamada do microservice fornecedor

--- src/main/java/br/com/lorencini/micorservice/loja/service/CompraService.java
+++ src/main/java/br/com/lorencini/micorservice/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.lorencini.micorservice.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,10 +11,12 @@ import br.com.lorencini.micorservice.loja.dto.InfoFornecedorDTO;
 
 @Service
 public class CompraService {
+	
+	@Autowired
+	private RestTemplate client;
 
 	public void realizaCompra(CompraDTO compra) {
 
-		RestTemplate client = new RestTemplate();
 		ResponseEntity<InfoFornecedorDTO> exchange = 
 				client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(),
 				HttpMethod.GET, null, InfoFornecedorDTO.class);
